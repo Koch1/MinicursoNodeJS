912fe49e1835e89f749a5b29130b0c3a741f81ce
Koch <matheuskoch13@gmail.com>
2019-10-16 13:35:26 -0300

    Parte inicial do minicurso

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+var node
+
+/* GET home page. */
+router.get('/index', function(req, res, next) {
+  res.render('index', {
+    title: 'Pagina Inicial',
+    nav: [{
+        id: '1',
+        nome: 'pedro',
+        sobreme: 'meida',
+        apelido: 'pedrinho'
+      },
+      {
+        id: '2',
+        nome: 'marco',
+        sobreme: 'antonio',
+        apelido: 'marco'
+      },
+      {
+        id: '3',
+        nome: 'mathias',
+        sobreme: 'gonsalves',
+        apelido: 'gon'
+      },
+    ]
+  });
+});
+
+module.exports = router;
